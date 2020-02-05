d9b928f5afad184a1bedfbb32bfdc046ab3f6b4f
jacki0 <jackio@tuta.io>
2020-02-05 22:24:16 +0300

    Изменил шифр

--- hidden.py
+++ hidden.py
@@ -1,6 +1,13 @@
 import hashlib
 import string
 
+
+alphabet = ''.join([chr(i) for i in range(ord('а'), ord('а') + 34)])
+alphabet = alphabet[: 6] + alphabet[-1] + alphabet[6: -2]
+alphabet += string.digits + string.ascii_letters + ' ' + string.punctuation + alphabet.upper()
+alphabet *= 3
+
+
 def keygen(message):
     key = hashlib.sha256(bytes(message, 'utf-8')).hexdigest()
     return key
@@ -8,12 +15,11 @@ def keygen(message):
 
 def encrypt(message, key):
     cryptmessage = ''
-    alphabet = ord('а')
-    alphabet = ''.join([chr(i) for i in range(alphabet, alphabet + 6)] + [chr(alphabet + 33)] + [chr(i) for i in range(alphabet + 6, alphabet + 32)])
-    alphabet += string.digits + string.ascii_letters + ' ' + string.punctuation + alphabet.upper()
     for letter in message:
-        i = alphabet.find(letter) * key
-        while i >= len(alphabet):
-            i -= len(alphabet) - 1
-        cryptmessage += alphabet[i]
+        i = alphabet.find(letter) + key
+        cryptmessage += alphabet[i * 2]
     return cryptmessage
+
+
+def decrypt(cryptmessage, key):
+    pass
