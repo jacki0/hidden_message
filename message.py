b9ded0a8c352f62be9d494a1b3545d45c1c35b13
jacki0 <jackio@tuta.io>
2020-11-18 01:06:59 +0300

    Changed the logic of interaction with the user. Fixed some syntax errors

--- message.py
+++ message.py
@@ -4,23 +4,23 @@ import random
 
 
 key = open('key.txt').read()
-'''bot = telebot.TeleBot(key)
+bot = telebot.TeleBot(key)
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Получить сообщение', 'Отправить сообщение')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, ("С помощью этого бота можно отправит анонимное сообщение которое самоуничтожится после прочтения.\
-                                       \nВ ответ на сообщение бот вернёт пароль для получения сообщения")
-    reply_markup=keyboard1)
+    bot.send_message(message.chat.id, "С помощью этого бота можно отправит анонимное сообщение которое самоуничтожится после прочтения.\
+                                       \nВ ответ на сообщение бот вернёт пароль для получения сообщения",\
+                                        reply_markup = keyboard1)
 
 @bot.message_handler(content_types=['text'])
 def passwordgeneration(message):
     if message.text.lower() == 'получить сообщение':
+        bot.send_message(message.chat.id, 'Для получения сообщения необходим пароль')
+    elif message.text.lower() == 'отправить сообщение':
         symbols = string.ascii_letters + string.digits
         password = ''.join(random.sample(symbols, random.randint(15,40)))
         bot.send_message(message.chat.id, password)
-    elif message.text.lower() == 'отправить сообщение':
-        pass
 
-bot.polling()'''
+bot.polling()
